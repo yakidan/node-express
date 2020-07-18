324f5893b6b9319c9da2a593d7825c510f1d30c6
Daniil Yak <8spider-man@mail.ru>
2020-07-18 18:44:56 +0300

    initial

---
+++ routes/profile.js
@@ -0,0 +1,32 @@
+const { Router } = require('express')
+const auth = require('../middleware/auth')
+const User = require('../models/user')
+const router = Router();
+
+router.get('/', async (req, res) => {
+    res.render('profile', {
+        title: 'Профиль',
+        isProfile: true,
+        user: req.user.toObject()
+    })
+})
+router.post('/', auth, async (req, res) => {
+    try {
+        const user = await User.findById(req.user._id)
+
+        const toChange = {
+            name: req.body.name
+        }
+        console.log(req.file)
+        if (req.file) {
+            toChange.avatarUrl = req.file.path
+        }
+        Object.assign(user, toChange)
+        await user.save()
+        res.redirect('/profile')
+    } catch (e) {
+        console.log(e)
+    }
+})
+
+module.exports = router
